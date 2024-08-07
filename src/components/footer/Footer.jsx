88a7cf4773b96abfe33616d36514c83ff43deda3
Soifiaouir <soifiaouir@gmail.com>
2024-08-07 10:55:42 +0200

    header + footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () =>{
+
+        return (
+            <p>on aime les pistaches et le</p>
+        );
+}
+
+export default Footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,48 @@
+import React, { useState} from "react";
+import Logofacc from '../../assets/Logofacc.png';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Navbar = () => {
+
+    const [showPastEvents, setShowPastEvents] = useState(false);
+    const [showPracticalInfo, setShowPracticalInfo] = useState(false);
+
+    return (
+        <nav className="menu">
+            <Link to="/" className="logo-link">
+                <img src={Logofacc} alt='logofacc' className="Facclogo"/>
+            </Link>
+            <Link to="/Edition2024">
+                <p><span>Edition 2024</span></p>
+            </Link>
+            <Link to="/Artistes">
+                <p><span>Artistes</span></p>
+            </Link>
+            <div className="nav-dropdown" onMouseEnter={() => setShowPastEvents(true)}  onMouseLeave={() => setShowPastEvents(false)}>
+                <span>Evenement Passé</span>
+                {showPastEvents && (
+                    <div className="dropdown">
+                        <p>2022</p>
+                        <p>2020</p>
+                        <p>2018</p>
+                        <p>2016</p>
+                        <p>2013</p>
+                    </div>
+                )}
+            </div>
+            <div className="nav-dropdown" onMouseEnter={() => setShowPracticalInfo(true)}  onMouseLeave={() => setShowPracticalInfo(false)}>
+                <span>Infos Pratiques</span>
+                {showPracticalInfo && (
+                    <div className="dropdown">
+                        <p>contact</p>
+                        <p>A propos</p>
+                    </div>
+                )}
+            </div>
+
+        </nav>
+    );
+};
+
+export default Navbar;
