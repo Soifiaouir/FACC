30ece99739c85dfb749832070a53ac53e8feff16
Soifiaouir <soifiaouir@gmail.com>
2024-08-08 12:07:26 +0200

    API

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const express = require('express');
 const path = require('path');
-const jsonServer = require('json-server');
-
 const app = express();
-const apiServer = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
 
-const PORT_APP = process.env.PORT_APP || 5080;
-const PORT_API = process.env.PORT_API || 5891;
+const PORT = process.env.PORT || 5080;
 
-// Configuration pour servir l'application React
+// Servir les fichiers statiques du dossier 'build'
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Pour toutes les autres requêtes, retourner le fichier 'index.html' du dossier 'build'
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Configuration pour servir l'API JSON
-apiServer.use(middlewares);
-apiServer.use(router);
-
-// Démarrer le serveur pour l'application React
-app.listen(PORT_APP, () => {
-  console.log(`React app is running on port ${PORT_APP}`);
-});
-
-// Démarrer le serveur pour l'API JSON
-apiServer.listen(PORT_API, () => {
-  console.log(`JSON API is running on port ${PORT_API}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
