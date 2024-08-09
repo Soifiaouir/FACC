ac15f5dfc2d9cbc9b5a13cf303901a3cbf1972d6
Soifiaouir <soifiaouir@gmail.com>
2024-08-09 11:26:06 +0200

    apienligne

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -4,14 +4,17 @@ import { BASE_URL } from "../config";
 //fonction asynchrone pour récuperer les donéées des artistes depuis l'api
 export const getArtistes = async () => {
     try {
-      const response = await fetch(`${BASE_URL}/artistes`);
+      const response = await fetch(BASE_URL);
+     
       if (!response.ok) {
         throw new Error('Erreur de récupération des données de l\'api');
       }
       const data = await response.json();
+      console.log('Data:', data);
       return data.artistes; // Accéder à la clé "artistes" dans le JSON
     } catch (error) {
       console.error('Erreur lors de la récupération des artistes:', error.message);
       throw error; // Relance l'erreur pour la gestion dans le composant
     }
   };
+
