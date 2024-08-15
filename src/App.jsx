fa396e69fa422b1709e7fd9e4fa0c30e401f3030
Soifiaouir <soifiaouir@gmail.com>
2024-08-15 15:59:44 +0200

    t

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import './App.css';
 import {Outlet} from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from './components/footer/Footer';
-import Edition2012 from './pages/2012/Edition2012';
-import CarousselU from './components/caroussels/carousselU/CarousselU';
-
 
 
 
